ece529a27019e879573d7ffbb0fb5ed9cfc7ea90
Rahul <rahulchavda6000@gmail.com>
2024-11-09 15:40:57 +0530

    add Stripe Payment Method

--- controllers/orderCantrollers.js
+++ controllers/orderCantrollers.js
@@ -1,8 +1,16 @@
-// placing Orders using COD method
-
 import orderModel from "../models/orderModel.js";
 import userModel from "../models/userModel.js";
+import Stripe from "stripe";
 
+// global variables
+
+const currency = "inr";
+const deliveryCharge = "100";
+
+// getway initialize
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+// placing Orders using COD method
 export const placeOrder = async (req, res) => {
   try {
     const { userId, items, amount, address } = req.body;
@@ -31,11 +39,81 @@ export const placeOrder = async (req, res) => {
 
 // placing Orders using Stripe method
 
-export const placeOrderStripe = async (req, res) => {};
+export const placeOrderStripe = async (req, res) => {
+  try {
+    const { userId, items, amount, address } = req.body;
+    const { origin } = req.headers;
 
-// placing Orders using Razorpay method
+    const orderData = {
+      userId,
+      items,
+      amount,
+      address,
+      paymentMethod: "Stripe",
+      payment: false,
+      date: Date.now(),
+    };
 
-export const placeOrderRazorpay = async (req, res) => {};
+    const newOrder = new orderModel(orderData);
+    await newOrder.save();
+
+    const line_items = items.map((item) => ({
+      price_data: {
+        currency: currency,
+        product_data: {
+          name: item.name,
+        },
+        unit_amount: item.price * 100,
+      },
+      quantity: item.quantity,
+    }));
+
+    line_items.push({
+      price_data: {
+        currency: currency,
+        product_data: {
+          name: "Delivery Charges",
+        },
+        unit_amount: deliveryCharge * 100,
+      },
+      quantity: 1,
+    });
+
+    const session = await stripe.checkout.sessions.create({
+      success_url: `${origin}/verify?success=true&orderId=${newOrder._id}`,
+      cancel_url: `${origin}/verify?success=false&orderId=${newOrder._id}`,
+      line_items,
+      mode: "payment",
+    });
+
+    res.json({ success: true, session_url: session.url });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// Verify Stripe
+
+export const verifyStripe = async (req, res) => {
+  const { orderId, success, userId } = req.body;
+
+  try {
+    if (success === "true") {
+      await orderModel.findByIdAndUpdate(orderId, { payment: true });
+      await userModel.findByIdAndUpdate(userId, { cartData: {} });
+
+      res.json({ success: true });
+    } else {
+      await orderModel.findOneAndDelete(orderId);
+      res.json({success:false})
+    }
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
 
 // All Orders Data for admin
 
@@ -71,7 +149,7 @@ export const updateOrdersStatus = async (req, res) => {
     const { orderId, status } = req.body;
 
     await orderModel.findByIdAndUpdate(orderId, { status });
-    res.json({success:true,message:"Status Updated"})
+    res.json({ success: true, message: "Status Updated" });
   } catch (error) {
     console.log(error);
     res.json({ success: false, message: error.message });
--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -1,23 +1,30 @@
 import express from "express";
-import { allOrders, placeOrder, placeOrderRazorpay, placeOrderStripe, updateOrdersStatus, userOrders } from "../controllers/orderCantrollers.js";
+import {
+  allOrders,
+  placeOrder,
+  placeOrderStripe,
+  updateOrdersStatus,
+  userOrders,
+  verifyStripe,
+} from "../controllers/orderCantrollers.js";
 import adminAuth from "../middleware/adminAuth.js";
 import userAuth from "../middleware/userAuth.js";
-
+import authUser from "../middleware/userAuth.js";
 
 const orderRouter = express.Router();
-
 
 // Admin Routes
 orderRouter.post("/list", adminAuth, allOrders);
 orderRouter.post("/status", adminAuth, updateOrdersStatus);
 
 // Payment Routes
-orderRouter.post("/place",userAuth,placeOrder)
-orderRouter.post("/stripe",userAuth,placeOrderStripe)
-orderRouter.post("/razorpay",userAuth,placeOrderRazorpay)
+orderRouter.post("/place", userAuth, placeOrder);
+orderRouter.post("/stripe", userAuth, placeOrderStripe);
 
 // User Routes
-orderRouter.post("/userorders",userAuth,userOrders)
+orderRouter.post("/userorders", userAuth, userOrders);
 
+// Verify Payment
+orderRouter.post("/verifystripe", authUser, verifyStripe);
 
-export default orderRouter
+export default orderRouter;
